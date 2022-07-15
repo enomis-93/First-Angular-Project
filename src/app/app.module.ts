f9253ed82c59d132adaf38571fe9a1a93821435c
Enomis9339 <61394403+Enomis9339@users.noreply.github.com>
2022-07-15 17:26:20 +0200

    Created quote box component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersTableComponent } from './users-table/users-table.component';
+import { QuoteBoxComponent } from './quote-box/quote-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersTableComponent
+    UsersTableComponent,
+    QuoteBoxComponent
   ],
   imports: [
     BrowserModule,
